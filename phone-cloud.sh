c72dafad87eaf84c5a0d1b45e624fd667bbb344c
shaheemp76m <shaheemp76m@gmail.com>
2026-06-16 10:12:05 +0530

    indroduction of veriables

--- phone-cloud.sh
+++ phone-cloud.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash 
 
+PHONE _IP=
+PHONE_PORT=2222
+USER=
+
 if mountpoint -q ~/cloud; then 
     if fusermount -uz ~/cloud; then
         notify-send "Phone Cloud" "Unmounted"
@@ -7,7 +11,7 @@ if mountpoint -q ~/cloud; then
         notify-send "Phone Cloud" "Unmount Failed"
     fi
 else 
-    if ! nc -z -w 1 100.94.139.101 >/dev/null 2>&1; then
+    if ! nc -z -w 1 "$PHONE_IP" "$PHONE_PORT" >/dev/null 2>&1; then
          notify-send "Phone Cloud" "phone not reachable"
          exit 1
     fi
@@ -22,8 +26,8 @@ else
     -o reconnect \
     -o ServerAliveInterval=5 \
     -o ServerAliveCountMax=2 \
-    -p 2222 \
-    smp76@100.94.139.101:/ \
+    -p "$PHONE_PORT" \
+    "$USER"@"$PHONE_IP":/ \
     ~/cloud; then
         notify-send "Phone Cloud" "mounted"
     else 
